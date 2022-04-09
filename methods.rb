e6070643161e25b6b003e761a560f7b13822eab7
Rahul Shah <rashah1998@yahoo.com>
2022-04-08 22:46:27 -0400

    commit tree 38891b37708b51caebff90015fb24330ef3cf9f9

--- methods.rb
+++ methods.rb
@@ -1,1 +1,25 @@
-# Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name="Hello, programmer!")
+    puts "Hello, #{name}!"
+end
+
+def add(x, y)
+    x + y
+end
+
+def halve(number)
+    if number.class == Integer
+        return  number/2
+    else
+        return nil
+    end
+end
+
+puts halve(3)
